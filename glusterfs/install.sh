452da691596f98e9927215cd80496d083c3d53fe
Sergey Panov <gostroy@gmail.com>
2013-06-14 11:47:49 +0300

    modifi glusterfs/install.sh

--- glusterfs/install.sh
+++ glusterfs/install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-FS=`df -h|grep glusterfs|awk '{print $7}'`
+FS=$(df -h|grep glusterfs|xargs|cut -d" " -f6)
+echo "Touch $FS/glusterfs"
 touch $FS/glusterfs
 
-ln -sv /opt/zabbix-plugins/zabbix_panser/glusterfs/panser_glusterfs.conf /etc/zabbix/zabbix_agentd.d/
+ln -svf /opt/zabbix-plugins/zabbix_panser/glusterfs/panser_glusterfs.conf /etc/zabbix/zabbix_agentd.d/
 service zabbix-agent restart
