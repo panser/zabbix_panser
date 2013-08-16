c33c9e689102e35484d7ce010f411770163d737c
Sergey Panov <gostroy@gmail.com>
2013-08-16 11:51:20 +0300

    modified:   glusterfs/bin/vfs_glusterfs_mounted.sh 	modified:   glusterfs/panser_glusterfs.conf

--- glusterfs/bin/vfs_glusterfs_mounted.sh
+++ glusterfs/bin/vfs_glusterfs_mounted.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-FS=$(cat /proc/mounts |grep gluster|cut -d" " -f2|uniq|head -1)
+glusterfsdir=/home/monline/cloud
+#FS=$(cat /proc/mounts |grep gluster|cut -d" " -f2|uniq|head -1)
+FS=${1:-$glusterfsdir}
+
 timeout -k 2 20 cat $FS/glusterfs > /dev/null 2>&1
 echo $?
+
